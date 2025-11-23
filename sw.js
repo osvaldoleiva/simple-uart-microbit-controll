aeb4366e86c3acb95a3434c3de4b65eec7a23b0b
osvaldoleivabarria <osvaldoleivabarria1@gmail.com>
2025-11-23 02:46:52 -0300

    minor fix

--- sw.js
+++ sw.js
@@ -1,41 +1,7 @@
-const CACHE_NAME = "genta-robot-cache-v1";
-const URLS_A_CACHEAR = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./img/genta-logo.svg",
-  "./img/icon-192.png",
-  "./img/icon-512.png",
-  "./img/icon-maskable-512.png",
-  "./img/splash-1280x1920.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_A_CACHEAR))
-  );
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((respuesta) => {
-      return respuesta || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (e) => {
+  clients.claim();
 });
